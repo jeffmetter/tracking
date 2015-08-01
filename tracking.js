463eaa05fb0a1e16d73394c7352bf6d4ab243f9c
jeffmetter <jmetter@midigitalagency.com>
2015-08-01 11:52:32 -0400

    test

--- tracking.js
+++ tracking.js
@@ -210,6 +210,19 @@
 
 
 
+var elements = document.getElementsByTagName("iframe");
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].src = "foo";
+/*
+	if (iframeurl && iframeurl.indexOf(mi_pu) > -1) {
+		if (iframeurl.indexOf("?") > -1) {
+			questamp = "&";
+		}
+		jQuery(this).attr("src", iframeurl+questamp+"track_fs="+track_fs+"&track_fm="+track_fm+"&First_Term="+first_term+"&track_fc="+track_fc+"&track_fn="+track_fn+"&track_ls="+track_ls+"&track_lm="+track_lm+"&track_lt="+track_lt+"&track_lc="+track_lc+"&track_ln="+track_ln);
+	}
+*/
+}​
 
 		// populate form handler
 		/*
