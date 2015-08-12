229d2a9c843bca0c76097c24887fd28fb2364cdf
jeffmetter <jmetter@midigitalagency.com>
2015-08-12 12:38:15 -0400

    t

--- referral.js
+++ referral.js
@@ -231,7 +231,7 @@
 	if (window.addEventListener) {
 		window.addEventListener("load", trackReferrals, false);
 	} else if (window.attachEvent) {
-		window.attachEvent("onload", trackReferrals;
+		window.attachEvent("onload", trackReferrals);
 	} else {
 		trackReferrals();
 	}
